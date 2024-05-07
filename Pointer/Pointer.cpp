2c8ed358a35e99e602e21c6f7e00f54be79aad29
Afnan Asilah <afnanyogs@gmail.com>
2024-05-07 10:57:23 +0700

    menambahkan output

--- Pointer/Pointer.cpp
+++ Pointer/Pointer.cpp
@@ -1,4 +1,25 @@
 #include <iostream>
 using namespace std;
 
-class mahasiswa
+class mahasiswa {
+public:
+	int nim;
+	void showNim() {
+		cout << "No induk = " << nim << endl;
+    }
+};
+
+int main() {
+
+	mahasiswa mhs{ 1 }; //object mhs
+	mhs.showNim(); //Member Access Operator
+
+	mahasiswa& refMhs = mhs; //Pointer Reference refMhs
+	refMhs.nim = 2; //Member Access Operator
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs; //pointer Derreference pMhs
+	pMhs->nim = 3; // Arrow operator
+	pMhs->showNim();
+	return 0;
+}
